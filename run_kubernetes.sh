fe4200206c0c1d510c6ee70607de14fd1c2dd943
N05h3ll <m.ashm985@gmail.com>
2020-09-18 01:42:08 +0200

    Added kubernetes_run script, kubernetes output file.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=n05h3ll/devops_microservices_project
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops-microservices-project --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops_microservices_project 8000:80
