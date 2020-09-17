60c1a35ff6971537581e8a9215e4fb0b9e760bdd
N05h3ll <m.ashm985@gmail.com>
2020-09-17 04:22:43 +0200

    Edited repo name.

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,7 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=n05h3ll/DevOps_Microservices_Project
+dockerpath=n05h3ll/devops_microservices_project
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
