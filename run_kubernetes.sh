2a46e95af630ea37afa47d8fd00f760dc1d42e83
N05h3ll <m.ashm985@gmail.com>
2020-09-18 14:44:50 +0200

    Echo sleep duration.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,12 +9,13 @@ dockerpath=n05h3ll/devops_microservices_project
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run devops-microservices-project --image=$dockerpath --port=8000
-
+# Sleep 20 seconds to allow the pod to be available
+echo "Waiting for pod to be available ..."
+sleep 20s
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-# Sleep 20 seconds to allow the pod to be available
-sleep 20s
+
 kubectl port-forward devops-microservices-project 8000:80
